31826c2686e65ab5b08e9236f194c4c7fe15d7b5
Michael Amiro <odindo.michael@gmail.com>
2020-07-01 09:08:20 +0300

    Initial Commit

---
+++ Credit Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Credit Card
+//
+//  Created by Michael Amiro on 01/07/2020.
+//  Copyright © 2020 Michael Amiro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
